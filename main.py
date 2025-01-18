6713379363beb51e966f5e103c7babe4d3beab88
SirRazac <Fabian.Kipp@web.de>
2025-01-18 19:59:47 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-from fastapi import FastAPI, HTTPException
-import Adafruit_DHT
+from fastapi import FastAPI
 from pydantic import BaseModel
 
 app = FastAPI()
-
-SENSOR = Adafruit_DHT.DHT22
-PIN = 4
 
 class SensorData(BaseModel):
     temperature: float
@@ -13,8 +9,5 @@ class SensorData(BaseModel):
 
 @app.get("/api/data", response_model=SensorData)
 async def get_sensor_data():
-    humidity, temperature = Adafruit_DHT.read_retry(SENSOR, PIN)
-    if humidity is not None and temperature is not None:
-        return SensorData(temperature=temperature, humidity=humidity)
-    else:
-        raise HTTPException(status_code=500, detail="Sensor read failed")
+    # Simulierte Sensordaten
+    return SensorData(temperature=25.0, humidity=60.0)
